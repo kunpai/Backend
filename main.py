7312fcb60fe0ff1f515ec51d8375729b142b5356
kunpai <kunpai@ucdavis.edu>
2022-04-21 18:57:34 -0700

    created db

--- main.py
+++ main.py
@@ -1,6 +1,26 @@
+import imp
 import pandas as pd
+import sqlite3
+import json
+from flask import Flask
+from flask import g
 
-data = pd.read_csv (r'C:\Users\Ron\Desktop\Test\products.csv')   
+data = pd.read_csv (r'codefoobackend_cfgames.csv')   
 df = pd.DataFrame(data)
 
-print(df)
+app = Flask(__name__)
+DATABASE = './database.db'
+
+def create_db():
+    db = getattr(g, '_database', None)
+    if db is None:
+        db = g._database = sqlite3.connect(DATABASE)
+        cur = db.cursor()
+        cur.execute(
+            'CREATE TABLE IF NOT EXISTS data (id TEXT media_type TEXT name TEXT short_name TEXT long_description TEXT short_description TEXT created_at TEXT updated_at TEXT review_url TEXT review_score TEXT slug TEXT genres TEXT created_by TEXT published_by TEXT franchises TEXT regions TEXT)')
+        db.commit()
+    db.row_factory = sqlite3.Row
+    return db
+
+
+
