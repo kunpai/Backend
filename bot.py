545dd5f5697eddc126789006bdadd493333ee1f3
kunpai <kunpai@ucdavis.edu>
2022-04-24 01:00:37 -0700

    bot commands

--- bot.py
+++ bot.py
@@ -14,6 +14,8 @@ intents.members = True
 client = discord.Client(intents=intents)
 slash = SlashCommand(client, sync_commands=True)
 members = []
+wishlist = {}
+
 
 @client.event
 async def on_ready():
@@ -30,7 +32,7 @@ async def _search(ctx=SlashContext, *, media=None):
     await ctx.send(embed=discord.Embed(title="Fetching information", description="", color=0xff0000))
     link = "http://127.0.0.1:5000/api/details"
     try:
-        retjson = requests.post(url=link, json={'slug': media})
+        retjson = requests.post(url=link, json={"slug": media})
         embed = discord.Embed(
             title=media.title(), description="", color=0xff0000)
         embed.set_author(name=ctx.author.name, icon_url=ctx.author.avatar_url)
@@ -41,7 +43,70 @@ async def _search(ctx=SlashContext, *, media=None):
         await ctx.send(embed=embed)
     except Exception as e:
         print(e)
-        notfound = discord.Embed(title="Course not found", color=0x00ff00)
+        notfound = discord.Embed(title="Media not found", color=0x00ff00)
+        return await ctx.send(embed=notfound)
+
+@slash.slash(name="recommend", description="Recommends media based on type and genre")
+async def _recommend(ctx=SlashContext, *, type = None, genre = None):
+    await ctx.send(embed=discord.Embed(title="Fetching information", description="", color=0xff0000))
+    link = "http://127.0.0.1:5000/api/recommend"
+    try:
+        retjson = requests.post(url=link, json={"media_type": type, "genres": genre})
+        embed = discord.Embed(
+            title="Recommendations", description="", color=0xff0000)
+        embed.set_author(name=ctx.author.name, icon_url=ctx.author.avatar_url)
+        for c in retjson:
+            print(c)
+            embed.add_field(name=type, value=f"> Title: {c['name']}\n> Media Type: {c['media_type']}\n> \
+                Description: {c['short_description']}\n> Genres: {c['genres']}\n> Ratings: {c['ratings']}\n> Review URL: {c['review_url']}", inline=False)
+        await ctx.send(embed=embed)
+
+    except Exception as e:
+        print(e)
+        notfound = discord.Embed(title="Media not found", color=0x00ff00)
+        return await ctx.send(embed=notfound)
+
+@slash.slash(name="publisher", description="Searches media based on media type and publisher")
+async def _publisher(ctx=SlashContext, *, type = None, publisher = None):
+    await ctx.send(embed=discord.Embed(title="Fetching information", description="", color=0xff0000))
+    link = "http://127.0.0.1:5000/api/recommend"
+    try:
+        retjson = requests.post(url=link, json={"media_type": type, "published_by": publisher})
+        embed = discord.Embed(
+            title="Results", description="", color=0xff0000)
+        embed.set_author(name=ctx.author.name, icon_url=ctx.author.avatar_url)
+        for c in retjson:
+            print(c)
+            embed.add_field(name=publisher, value=f"> Title: {c['name']}\n> Media Type: {c['media_type']}\n> \
+                Description: {c['short_description']}\n> Genres: {c['genres']}\n> Ratings: {c['ratings']}\n> Review URL: {c['review_url']}", inline=False)
+        await ctx.send(embed=embed)
+
+    except Exception as e:
+        print(e)
+        notfound = discord.Embed(title="Media not found", color=0x00ff00)
+        return await ctx.send(embed=notfound)
+
+@slash.slash(name="add", description="Adds media to your wishlist")
+async def _add(ctx=SlashContext, *, media=None):
+    await ctx.send(embed=discord.Embed(title="Fetching information", description="", color=0xff0000))
+    link = "http://127.0.0.1:5000/api/details"
+    try:
+        retjson = requests.post(url=link, json={"slug": media})
+        embed = discord.Embed(
+            title=media.title(), description="", color=0xff0000)
+        embed.set_author(name=ctx.author.name, icon_url=ctx.author.avatar_url)
+        for c in retjson:
+            print(c)
+            embed.add_field(name=media, value=f"> Title: {c['name']}\n> Media Type: {c['media_type']}\n> \
+                Description: {c['short_description']}\n> Genres: {c['genres']}\n> Ratings: {c['ratings']}\n> Review URL: {c['review_url']}", inline=False)
+        await ctx.send(embed=embed)
+        wishlist[ctx.author].append(c)
+    except KeyError:
+        wishlist[ctx.author] = []
+        wishlist[ctx.author].append(c)
+    except Exception as e:
+        print(e)
+        notfound = discord.Embed(title="Media not found", color=0x00ff00)
         return await ctx.send(embed=notfound)
 
 
